d017b9176ece4c5738301a5b9a0f4207fb672b24
CynthiaAlwenge <munyivacynthier@gmail.com>
2014-06-07 14:20:06 +0300

    Create array_list.php

---
+++ array_list.php
@@ -0,0 +1,12 @@
+<?php
+// Array of Friends
+return $friends = array(
+    array('name' => 'philip', 'age' => '20', 'phone' => '0723280083'),
+    array('name' => 'philgon', 'age' => '18', 'phone' => '0712289096'),
+    array('name' => 'mwangi', 'age' => '21', 'phone' => '0755882538'),
+    array('name' => 'munyiva', 'age' => '19', 'phone' => '0714655959'),
+    array('name' => 'maxwel', 'age' => '23', 'phone' => '0713856990'),
+    array('name' => 'nicholus', 'age' => '17', 'phone' => '0710082855'),
+
+  );
+?>
