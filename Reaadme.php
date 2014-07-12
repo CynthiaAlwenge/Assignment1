da6e4a42eef85299c2cf3420c3bf41fa4e8e637c
CynthiaAlwenge <munyivacynthier@gmail.com>
2014-07-12 15:50:51 +0300

    Create Reaadme.php

---
+++ Reaadme.php
@@ -0,0 +1,14 @@
+## Sartuday Hack
+### User Application
+
+Database structure
+
+
+1. Names
+2.Date of Birth
+3.Country -Kenya
+4.ID Number
+5.Phone
+6.Gender -Female/Male
+7.Marital Status (Single, Married, Divorced, Engaged, Complicated)
+8.Photo
